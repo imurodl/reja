f425ed529c464d6098c49cd55273545ce23ab453
Murod Mirzaliev <murodlearns@gmail.com>
2024-11-13 21:00:37 +0900

    fix: html and fix server

--- server.js
+++ server.js
@@ -12,18 +12,20 @@ app.use(express.urlencoded({ extended: true }));
 
 // 3: Views code
 app.set("views", "views");
-app.set("views engine", "ejs"); // BSSR
+app.set("view engine", "ejs"); // BSSR
 
 // 4: Routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1 style="background: #3bbf5e">HELLO WORLD<h1>`);
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
-app.get("/gift", function (req, res) {
-  res.end("Siz sovg'alar bolimidasiz");
+
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
-let PORT = 3000;
+let PORT = 3001;
 server.listen(PORT, function () {
   console.log(`The server is running succesfully on port: ${PORT}`);
 });
