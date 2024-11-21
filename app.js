26780deb8ad0830443d0afa74ac211d545779fc1
Murod Mirzaliev <murodlearns@gmail.com>
2024-11-21 17:09:33 +0900

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,47 @@
+console.log("Web serverni boshlang");
+const express = require("express");
+const app = express();
+
+// for author js
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+// for author js
+
+// MongoDB chaqirish
+const db = require("./server").db();
+
+// MongoDB connect
+
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: Session code
+
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs"); // BSSR
+
+// 4: Routing code
+app.post("/create-item", (req, res) => {
+  // TODO: code with db here
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+  res.render("reja");
+});
+
+module.exports = app;
