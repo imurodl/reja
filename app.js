6fab608a36105cc2472329c50b8a9ed5d9aab4c6
Murod Mirzaliev <murodlearns@gmail.com>
2024-11-21 18:52:12 +0900

    feat: CRUD operations

--- app.js
+++ app.js
@@ -33,7 +33,16 @@ app.set("view engine", "ejs"); // BSSR
 
 // 4: Routing code
 app.post("/create-item", (req, res) => {
-  // TODO: code with db here
+  console.log("user entered /");
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      req.end("something went wrong");
+    } else {
+      res.end("successfully added");
+    }
+  });
 });
 
 app.get("/author", (req, res) => {
@@ -41,7 +50,17 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("user entered /");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        res.render("reja", { items: data });
+      }
+    });
 });
 
 module.exports = app;
