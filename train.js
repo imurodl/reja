e9272695ef5d53fd59c1b4b303fe8b2e8d9c6683
Murod Mirzaliev <murodlearns@gmail.com>
2024-11-21 21:47:17 +0900

    feat: B-Task solutions

--- train.js
+++ train.js
@@ -1,108 +1,6 @@
-// 21. Event loop & Calback functions
+// ------------ A-TASK --------------
 
-// console.log("Jacj Ma maslahatlari");
-
-// const list = [
-//   "yaxshi talaba bo'ling", // 0-20
-//   "to'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
-//   "o'zingizga ishlashni boshlang", // 30-40
-//   "siz kuchli bo'lgan narsani qiling", // 40-50
-//   "yoshlarga investitsiya qiling", // 50-60
-//   "endi dam oling", // 60
-// ];
-
-/*
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("insert number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (20 < a && a <= 30) callback(null, list[1]);
-  else if (30 < a && a <= 40) callback(null, list[2]);
-  else if (40 < a && a <= 50) callback(null, list[3]);
-  else if (50 < a && a <= 60) callback(null, list[4]);
-  else {
-    setTimeout(function () {
-      callback(null, list[5]);
-    }, 5000);
-  }
-}
-
-console.log("passed here 0");
-
-maslahatBering(90, (err, data) => {
-  if (err) console.log("ERROR:", err);
-  else console.log("javob:", data);
-});
-
-console.log("passed here 1");
-*/
-
-// 22. Asynchronous functions
-
-// async function maslahatBering(a) {
-//   if (typeof a !== "number") throw new Error("insert a number");
-//   else if (a <= 20) return list[0];
-//   else if (20 < a && a <= 30) return list[1];
-//   else if (30 < a && a <= 40) return list[2];
-//   else if (40 < a && a <= 50) return list[3];
-//   else if (50 < a && a <= 60) return list[4];
-//   else {
-//     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve(list[5]);
-//       }, 5000);
-//     });
-//   }
-// }
-
-// call via then/catch
-// console.log("passed here 0");
-// maslahatBering(25)
-//   .then((data) => {
-//     console.log("javob:", data);
-//   })
-//   .catch((err) => {
-//     console.log("ERROR:", err);
-//   });
-// console.log("passed here 1");
-
-// call via async/await
-// async function run() {
-//   let javob = await maslahatBering(25);
-//   console.log(javob);
-//   javob = await maslahatBering(71);
-//   console.log(javob);
-//   javob = await maslahatBering(42);
-//   console.log(javob);
-// }
-// run();
-
-// => bu yerda callback hell hosil bo'ladi va bundan qutulish uchun await ni ishlatamiz.
-
-// maslahatBering(25)
-//   .then((data) => {
-//     maslahatBering(35)
-//       .then((data) => {
-//         maslahatBering(40)
-//           .then((data) => {
-//             console.log("javob:", data);
-//           })
-//           .catch((err) => {
-//             console.log("ERROR:", err);
-//           });
-//         console.log("javob:", data);
-//       })
-//       .catch((err) => {
-//         console.log("ERROR:", err);
-//       });
-//     console.log("javob:", data);
-//   })
-//   .catch((err) => {
-//     console.log("ERROR:", err);
-//   });
-
-// A-TASK
-
-/*
+/* // Core function
 function countLetter(ltr, word) {
   let a = 0;
 
@@ -112,9 +10,10 @@ function countLetter(ltr, word) {
   return console.log(a);
 }
 
-countLetter("m", "mathematics");
+countLetter("6", "irogjore6opgfo6opfdg6");
 */
 
+/* // async/await solution
 async function countLetter(ltr, word) {
   if (typeof ltr !== "string" || ltr.length !== 1) {
     throw new Error("First argument must be a single letter");
@@ -132,13 +31,13 @@ async function countLetter(ltr, word) {
 
 async function run() {
   try {
-    let result = await countLetter("m", "mathematics");
+    let result = await countLetter("m", "Mathematics");
     console.log("result:", result);
 
-    result = await countLetter("z", "mathematics");
+    result = await countLetter("6", "mathematics");
     console.log("result:", result);
 
-    result = await countLetter("45", "mathematics");
+    result = await countLetter("4", "35748349785");
     console.log("result:", result);
   } catch (err) {
     console.log("error:", err.message);
@@ -146,3 +45,81 @@ async function run() {
 }
 
 run();
+*/
+
+//  ------------------- B-TASK: -----------------------
+
+// Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+// MASALAN countDigits("ad2a54y79wet0sfgb4") 7ni return qiladi.
+
+/* // Solution 1: core logic
+function countDigits(ltr) {
+  if (ltr.length === 0 || typeof ltr == "number") {
+    console.log("Insert a string");
+  } else {
+    let count = 0;
+    for (let i = 0; i < ltr.length; i++) {
+      for (let l = 0; l <= 9; l++) {
+        if (l == ltr[i]) count++;
+      }
+    }
+    return count;
+  }
+}
+const result = countDigits("ad2a54y79wet0sfgb98");
+console.log("result:", result);
+*/
+
+/* // Solution 2: with callback function
+function countDigits(ltr, callback) {
+  if (ltr.length === 0 || typeof ltr == "number")
+    callback("Insert a string", null);
+  else {
+    let count = 0;
+    for (let i = 0; i < ltr.length; i++) {
+      for (let l = 0; l <= 9; l++) {
+        if (l == ltr[i]) count++;
+      }
+    }
+    callback(null, count);
+  }
+}
+
+countDigits("ad2a54y79wet0sfgb98", (err, data) => {
+  if (err) console.log("error:", err);
+  else console.log("javob:", data);
+});
+*/
+
+/* // Solution 3: wish async/await multiple results
+async function countDigits(ltr, callback) {
+  if (ltr.length === 0 || typeof ltr == "number")
+    throw new Error("Insert a string");
+  else {
+    let count = 0;
+    for (let i = 0; i < ltr.length; i++) {
+      for (let l = 0; l <= 9; l++) {
+        if (l == ltr[i]) count++;
+      }
+    }
+    return count;
+  }
+}
+
+async function countResult() {
+  try {
+    let result1 = await countDigits("ad2a54y79wet0sfgb98");
+    console.log("result:", result1);
+
+    result1 = await countDigits("");
+    console.log("result:", result1);
+
+    result1 = await countDigits(1213214342);
+    console.log("result:", result1);
+  } catch (err) {
+    console.log("error:", err.message);
+  }
+}
+
+countResult();
+*/
