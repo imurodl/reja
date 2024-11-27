b860fc1f5097945db82a59092e1a3ccebb2bbddb
Murod Mirzaliev <murodlearns@gmail.com>
2024-11-27 21:42:04 +0900

    fix: fixed issues on updating

--- public/browser.js
+++ public/browser.js
@@ -1,9 +1,9 @@
 const createField = document.getElementById("create-field");
 
-function itemTeplate(item) {
+function itemTemplate(item) {
   return `
   <li class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
-    <span class="item-text">${item.reja}></span>
+    <span class="item-text">${item.reja}</span>
       <div>
         <button data-id='${item._id}' class="edit-me btn btn-secondary btn-sm mr-1">
             O'zgartirish
@@ -24,7 +24,7 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
     .then((response) => {
       document
         .getElementById("item-list")
-        .insertAdjacentElement("beforeend", itemTemplate(response.data));
+        .insertAdjacentHTML("beforeend", itemTemplate(response.data));
       createField.value = "";
       createField.focus();
     })
